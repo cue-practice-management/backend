d8202be41a8b86ea1b4fca84f6e9ebb37e7310a5
Juan Gerardo <jmendez1025@cue.edu.co>
2025-04-18 09:49:22 -0500

    feat: user new attributes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@users/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { validationSchema } from './common/config/env-validation.config';
+import { EnvironmentConfigService } from './common/config/environment-config.service';
+import { AppLogger } from '@common/loggers/app.logger';
+import { GlobalExceptionFilter } from '@common/filters/http-exception.filter';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(
-      {
-        isGlobal: true,
-        envFilePath: '.env'
-      }
-    ),
-    MongooseModule.forRootAsync(
-      {
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          uri: configService.get<string>('MONGO_URI'),
-          dbName: configService.get<string>('MONGO_DB_NAME')
-        })
-      }
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+      expandVariables: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [CommonModule],
+      inject: [EnvironmentConfigService],
+      useFactory: (env: EnvironmentConfigService) => ({
+        uri: env.mongoUri,
+        dbName: env.mongoDbName,
+      }),
+    }),
     UserModule,
     AuthModule,
-    CommonModule
+    CommonModule,
   ],
-  controllers: [],
-  providers: [],
+  providers:[
+    AppLogger,
+    {
+      provide: 'APP_FILTER',
+      useClass: GlobalExceptionFilter,
+    },
+  ]
 })
-export class AppModule { }
+export class AppModule {}
