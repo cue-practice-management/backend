c2d96ac41a9d2c5d981d13ea1dfa2147d41b40c4
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-09 21:26:57 -0500

    feat: faculty dtos

---
+++ src/faculty/dtos/faculty-response.dto.ts
@@ -0,0 +1,9 @@
+export class FacultyResponseDto {
+    _id: string;
+    name: string;
+    description: string;
+    deanName: string;
+    deanEmail: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
