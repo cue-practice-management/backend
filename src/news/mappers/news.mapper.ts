3b1ee89e67059abdb9b521239cd2031a0d1a6fb4
Juan Gerardo <jmendez1025@cue.edu.co>
2025-05-31 21:58:47 -0500

    feat(news): dto mapper

---
+++ src/news/mappers/news.mapper.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PaginateResult } from 'mongoose';
+import { News, NewsDocument } from '../schemas/news.schema';
+import { NewsResponseDto } from 'news/dtos/news-response.dto';
+import { PaginatedResult } from '@common/types/paginated-result';
+
+@Injectable()
+export class NewsMapper {
+  toNewsResponseDto(news: News): NewsResponseDto {
+    return {
+      id: news._id.toString(),
+      title: news.title,
+      summary: news.summary,
+      content: news.content,
+      coverImageUrl: news.coverImageUrl,
+      tags: news.tags,
+      createdAt: news.createdAt,
+      updatedAt: news.updatedAt,
+    };
+  }
+
+  toNewsResponsePaginatedDto(
+    paginatedResult: PaginateResult<NewsDocument>,
+  ): PaginatedResult<NewsResponseDto> {
+    return {
+      docs: paginatedResult.docs.map((news) =>
+        this.toNewsResponseDto(news),
+      ),
+      totalDocs: paginatedResult.totalDocs,
+      totalPages: paginatedResult.totalPages,
+      page: paginatedResult.page,
+      limit: paginatedResult.limit,
+    };
+  }
+}
