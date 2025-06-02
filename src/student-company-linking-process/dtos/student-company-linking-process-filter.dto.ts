248d6eeb9802d94182d7a826896516cd3fe677d1
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-02 15:34:15 -0500

    feat(linking): add filter DTO and custom exception for status change restrictions

---
+++ src/student-company-linking-process/dtos/student-company-linking-process-filter.dto.ts
@@ -0,0 +1,17 @@
+import { PaginationQueryDto } from "@common/dtos/pagination-query.dto";
+import { IsEnum, IsMongoId, IsOptional } from "class-validator";
+import { StudentCompanyLinkingProcessStatus } from "student-company-linking-process/enums/student-company-linking-process-status.enum";
+
+export class StudentCompanyLinkingProcessFilterDto extends PaginationQueryDto {
+  @IsOptional()
+  @IsMongoId()
+  student?: string;
+
+  @IsOptional()
+  @IsMongoId()
+  company?: string;
+
+  @IsOptional()
+  @IsEnum(StudentCompanyLinkingProcessStatus)
+  status?: StudentCompanyLinkingProcessStatus;
+}
---
+++ src/student-company-linking-process/exceptions/student-company-linking-process-approved-status-changed.exception.ts
@@ -0,0 +1,12 @@
+import { BaseHttpException } from "@common/exceptions/base-http.exception";
+import { STUDENT_COMPANY_LINKING_PROCESS_EXCEPTIONS } from "student-company-linking-process/constants/student-company-linking-process.constants";
+
+export class StudentCompanyLinkingProcessApprovedStatusChangedException extends BaseHttpException {
+    constructor() {
+        super(
+            STUDENT_COMPANY_LINKING_PROCESS_EXCEPTIONS.APPROVED_STATUS_ALREADY_CHANGED.code,
+            STUDENT_COMPANY_LINKING_PROCESS_EXCEPTIONS.APPROVED_STATUS_ALREADY_CHANGED.message,
+            STUDENT_COMPANY_LINKING_PROCESS_EXCEPTIONS.APPROVED_STATUS_ALREADY_CHANGED.statusCode
+        );
+    }
+}
