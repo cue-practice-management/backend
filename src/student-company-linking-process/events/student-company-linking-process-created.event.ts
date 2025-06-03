9b3a7ecac37d54d01ff8a7c33a34f7c8d9cf820d
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-02 22:06:15 -0500

    feat(linking): add events, listeners and event constants for student-company linking process

---
+++ src/student-company-linking-process/events/student-company-linking-process-created.event.ts
@@ -0,0 +1,7 @@
+import { StudentCompanyLinkingProcessResponseDto } from "student-company-linking-process/dtos/student-company-linking-process-response.dto";
+
+export class StudentCompanyLinkingProcessCreatedEvent {
+    constructor(
+        public readonly studentCompanyLinkingProcess: StudentCompanyLinkingProcessResponseDto,
+    ) {}
+}
