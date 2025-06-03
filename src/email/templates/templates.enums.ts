c13dbcb319e8e7ef8722cca2c1eaee8e47d7c53c
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-02 22:06:37 -0500

    feat(email): add template enums and payload structure for linking-related notifications

--- src/email/templates/templates.enums.ts
+++ src/email/templates/templates.enums.ts
@@ -2,4 +2,5 @@ export enum SesTemplates {
   OTP_RECOVERY = 'OtpRecovery',
   PASSWORD_UPDATED = 'PasswordUpdated',
   WELCOME = 'WelcomeTemplate',
+  STUDENT_COMPANY_LINKING_PROCESS_CREATED = 'StudentCompanyLinkingProcessCreated',
 }
