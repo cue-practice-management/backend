374108027812a5c576b2e5f1d860eeb5bb0912dc
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-03 17:41:34 -0500

    refactor(contract): set default status and register listener/mapper in contract module

--- src/student-company-contract/student-company-contract.module.ts
+++ src/student-company-contract/student-company-contract.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { StudentCompanyContractService } from './student-company-contract.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentCompanyContract, StudentCompanyContractSchema } from './schemas/student-company-contract.schema';
+import { StudentCompanyContractMapper } from './mappers/student-company-contract.mapper';
+import { StudentCompanyContractListener } from './listeners/student-company-contract.listener';
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { StudentCompanyContract, StudentCompanyContractSchema } from './schemas/
       }
     ])
   ],
-  providers: [StudentCompanyContractService]
+  providers: [StudentCompanyContractService, StudentCompanyContractMapper, StudentCompanyContractListener],
+  exports: [StudentCompanyContractService, StudentCompanyContractMapper]
 })
 export class StudentCompanyContractModule {}
