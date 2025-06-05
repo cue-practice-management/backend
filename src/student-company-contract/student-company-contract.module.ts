bf964eb06f2275bbfb8b4af61370cc9765089e56
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-05 14:12:47 -0500

    chore(contract): register module dependencies and controller for student-company contracts

--- src/student-company-contract/student-company-contract.module.ts
+++ src/student-company-contract/student-company-contract.module.ts
@@ -4,6 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StudentCompanyContract, StudentCompanyContractSchema } from './schemas/student-company-contract.schema';
 import { StudentCompanyContractMapper } from './mappers/student-company-contract.mapper';
 import { StudentCompanyContractListener } from './listeners/student-company-contract.listener';
+import { StudentCompanyContractController } from './student-company-contract.controller';
+import { StudentModule } from 'student/student.module';
+import { CompanyModule } from 'company/company.module';
+import { FileModule } from 'file/file.module';
+import { AuthModule } from '@auth/auth.module';
+import { EmailModule } from 'email/email.module';
 
 @Module({
   imports: [
@@ -12,9 +18,15 @@ import { StudentCompanyContractListener } from './listeners/student-company-cont
         name: StudentCompanyContract.name,
         schema: StudentCompanyContractSchema
       }
-    ])
+    ]),
+    StudentModule,
+    CompanyModule,
+    FileModule,
+    AuthModule,
+    EmailModule
   ],
   providers: [StudentCompanyContractService, StudentCompanyContractMapper, StudentCompanyContractListener],
-  exports: [StudentCompanyContractService, StudentCompanyContractMapper]
+  exports: [StudentCompanyContractService, StudentCompanyContractMapper],
+  controllers: [StudentCompanyContractController]
 })
-export class StudentCompanyContractModule {}
+export class StudentCompanyContractModule { }
