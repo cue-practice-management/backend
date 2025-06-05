e03ef9cf276f7b7e52768f7808f6fb13aa6dcb2e
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-05 14:14:12 -0500

    feat(contract): emit event on contract activation to trigger side-effects

---
+++ src/student-company-contract/events/student-company-contract-activated.event.ts
@@ -0,0 +1,7 @@
+import { StudentCompanyContractResponseDto } from "student-company-contract/dtos/student-company-contract-response.dto";
+
+export class StudentCompanyContractActivatedEvent {
+    constructor(
+        public readonly studentCompanyContract: StudentCompanyContractResponseDto,
+    ) {}
+}
