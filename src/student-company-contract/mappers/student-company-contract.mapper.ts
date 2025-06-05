ea049ec07706f49b28d7a9c930331ab7f93ad11f
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-05 14:12:19 -0500

    feat(contract): map contracts with signed URL and nested relations in response DTO

--- src/student-company-contract/mappers/student-company-contract.mapper.ts
+++ src/student-company-contract/mappers/student-company-contract.mapper.ts
@@ -1,12 +1,63 @@
+import { PaginatedResult } from "@common/types/paginated-result";
 import { Injectable } from "@nestjs/common";
+import { CompanyMapper } from "company/mappers/company.mapper";
+import { Company } from "company/schemas/company.schema";
+import { FileService } from "file/file.service";
+import { PaginateResult } from "mongoose";
 import { StudentCompanyContractResponseDto } from "student-company-contract/dtos/student-company-contract-response.dto";
 import { StudentCompanyContract } from "student-company-contract/schemas/student-company-contract.schema";
+import { StudentMapper } from "student/mapper/student.mapper";
+import { Student } from "student/schemas/student.schema";
 
 @Injectable()
 export class StudentCompanyContractMapper {
-    toStudentCompanyContractResponseDto(entity: StudentCompanyContract): StudentCompanyContractResponseDto {
-        return {
-            _id: entity._id.toString(),
-        };
-    }
+      constructor(private readonly fileService: FileService, private readonly studentMapper: StudentMapper, private readonly companyMapper: CompanyMapper) {}
+    
+    async toStudentCompanyContractResponseDto(
+        contract: StudentCompanyContract): Promise<StudentCompanyContractResponseDto> {
+            let signedUrl: string | undefined = undefined;
+
+            if (contract.contractUrl) {
+                signedUrl = await this.fileService.getPrivateFileSignedUrl(
+                    contract.contractUrl,
+                );
+            }
+
+            return {
+                _id: contract._id.toString(),
+                student: this.studentMapper.toStudentBasicInfoResponseDto(contract.student as unknown as Student ),
+                company: this.companyMapper.toCompanyBasicInfoResponseDto(contract.company as unknown as Company),
+                contractUrl: signedUrl,
+                startDate: contract.startDate,
+                endDate: contract.endDate,
+                status: contract.status,
+                isPaid: contract.isPaid,
+                cancellationReason: contract.cancellationReason,
+                cancellationDate: contract.cancellationDate,
+                createdAt: contract.createdAt,
+                updatedAt: contract.updatedAt,
+            };
+        }
+
+
+        async toStudentCompanyContractsPaginatedResponseDto(
+            contracts: PaginateResult<StudentCompanyContract>,
+        ): Promise<PaginatedResult<StudentCompanyContractResponseDto>> {
+            const items = await Promise.all(
+                contracts.docs.map((contract) =>
+                    this.toStudentCompanyContractResponseDto(contract),
+                ),
+            );
+
+            return {
+                docs: items,
+                totalDocs: contracts.totalDocs,
+                totalPages: contracts.totalPages,
+                page: contracts.page,
+                limit: contracts.limit,
+            };
+        }
+
+
+
 }
