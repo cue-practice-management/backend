f4c966c22c56b1f3c6225bd029e9233c4b11d316
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-05 14:14:05 -0500

    feat(contract): add DTOs for activation, cancellation and filtering of student-company contracts

---
+++ src/student-company-contract/dtos/activate-student-company-contract-request.dto.ts
@@ -0,0 +1,18 @@
+import { Type } from "class-transformer";
+import { IsBoolean, IsDate, IsNotEmpty } from "class-validator";
+
+export class ActivateStudentCompanyContractRequestDto {
+    @IsNotEmpty()
+    @Type(() => Date)
+    @IsDate()
+    startDate: Date;
+
+    @IsNotEmpty()
+    @Type(() => Date)
+    @IsDate()
+    endDate: Date;
+
+    @IsBoolean()
+    @Type(() => Boolean)
+    isPaid: boolean;
+}
---
+++ src/student-company-contract/dtos/cancel-student-company-contract-request.dto.ts
@@ -0,0 +1,17 @@
+import { Type } from "class-transformer";
+import { IsDate, IsEnum, IsNotEmpty, IsString } from "class-validator";
+import { StudentCompanyContractCancelledBy } from "student-company-contract/enums/student-company-contract-cancelled-by.enum";
+
+export class CancelStudentCompanyContractRequestDto {
+  @IsString()
+  @IsNotEmpty()
+  cancellationReason: string;
+
+  @IsEnum(StudentCompanyContractCancelledBy)
+  cancelledBy: StudentCompanyContractCancelledBy;
+
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  cancellationDate: Date;
+}
