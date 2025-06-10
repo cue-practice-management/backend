b007dea1778a6136c804b424c44dffb14c161f4d
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-10 06:09:23 -0500

    refactor(practice-template): move service to services/ and adjust imports

--- src/practice-template/practice-template.controller.ts
+++ src/practice-template/practice-template.controller.ts
@@ -9,7 +9,6 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { PracticeTemplateService } from './practice-template.service';
 import { AuthGuard } from '@auth/guards/auth.guard';
 import { RoleGuard } from '@auth/guards/role.guard';
 import { UserRole } from '@common/enums/role.enum';
@@ -21,6 +20,7 @@ import { PracticeTemplateFilterDto } from './dtos/practice-template-filter.dto';
 import { PaginatedResult } from '@common/types/paginated-result';
 import { ParseObjectIdPipe } from '@nestjs/mongoose';
 import { UpdatePracticeTemplateRequestDto } from './dtos/update-practice-template-request.dto';
+import { PracticeTemplateService } from './services/practice-template.service';
 
 @Controller('practice-templates')
 export class PracticeTemplateController {
--- src/practice-template/practice-template.module.ts
+++ src/practice-template/practice-template.module.ts
@@ -1,23 +1,35 @@
 import { Module } from '@nestjs/common';
-import { PracticeTemplateService } from './practice-template.service';
 import { PracticeTemplateController } from './practice-template.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PracticeTemplate, PracticeTemplateSchema } from './schemas/practice-template.schema';
 import { AuthModule } from '@auth/auth.module';
 import { FileModule } from 'file/file.module';
 import { PracticeTemplateMapper } from './mappers/practice-template-mapper';
+import { PracticeTemplateService } from './services/practice-template.service';
+import { PracticeTemplateDeliverable, PracticeTemplateDeliverableSchema } from './schemas/practice-template-deliverable.schema';
+import { PracticeTemplateFormat, PracticeTemplateFormatSchema } from './schemas/practice-template-format.schema';
+import { PracticeTemplateDeliverableService } from './services/practice-template-deliverable.service';
+import { PracticeTemplateDeliverableMapper } from './mappers/practice-template-deliverable.mapper';
 
 @Module({
   imports: [MongooseModule.forFeature([
     {
       name: PracticeTemplate.name,
       schema: PracticeTemplateSchema,
+    },
+    {
+      name: PracticeTemplateDeliverable.name,
+      schema: PracticeTemplateDeliverableSchema,
+    },
+    {
+      name: PracticeTemplateFormat.name,
+      schema: PracticeTemplateFormatSchema,
     }
   ]),
     AuthModule,
     FileModule
   ],
-  providers: [PracticeTemplateService, PracticeTemplateMapper],
+  providers: [PracticeTemplateService, PracticeTemplateMapper, PracticeTemplateDeliverableMapper ,PracticeTemplateDeliverableService],
   controllers: [PracticeTemplateController],
 })
 export class PracticeTemplateModule { }
