a81dabb5aff90bf12952d2816c6cd7aa4fbe39a1
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-11 17:34:26 -0500

    feat(constants): define populate options for practice definition with academic program and template relations

--- src/practice-definition/constants/practice-definition.constants.ts
+++ src/practice-definition/constants/practice-definition.constants.ts
@@ -1,4 +1,6 @@
+import { ACADEMIC_PROGRAM_POPULATE_OPTIONS } from "@academic-program/constants/academic-program.constants";
 import { HttpStatus } from "@nestjs/common";
+import { PRACTICE_TEMPLATE_POPULATE_OPTIONS } from "practice-template/constants/practice-template.constants";
 
 export const PRACTICE_DEFINITION_EXCEPTIONS = {
     NOT_FOUND: {
@@ -13,4 +15,21 @@ export const PRACTICE_DEFINITION_CONTRAINTS = {
         MIN: 1,
         MAX: 12
     }
+}
+
+export const PRACTICE_DEFINITION_POPULATE_OPTIONS = {
+    ACADEMIC_PROGRAM: {
+        path: 'academicProgram',
+        populate: [
+            ACADEMIC_PROGRAM_POPULATE_OPTIONS.FACULTY
+        ]
+    },
+    PRACTICE_TEMPLATE: {
+        path: 'practiceTemplate',
+        populate: [
+            PRACTICE_TEMPLATE_POPULATE_OPTIONS.DELIVERABLES,
+            PRACTICE_TEMPLATE_POPULATE_OPTIONS.FORMATS
+        ]
+    }
+
 }
