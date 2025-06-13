787de562d2b38f14c2895f5682ee3bc402b10975
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-13 12:53:15 -0500

    feat(practice-process): define exception codes for process validation

--- src/practice-process/constants/practice-process-deliverable.constants.ts
+++ src/practice-process/constants/practice-process-deliverable.constants.ts
@@ -1,3 +1,22 @@
+export const PRACTICE_PROCESS_EXCEPTIONS = {
+    PRACTICE_PROCESS_NOT_FOUND: {
+        code: 'PRACTICE_PROCESS_NOT_FOUND',
+        message: 'Practice process not found.',
+        status: 404,
+    },
+    STUDENT_HAS_NOT_COMPANY: {
+        code: 'STUDENT_HAS_NOT_COMPANY',
+        message: 'Student does not have a company assigned.',
+        status: 400,
+    },
+    STUDENT_HAS_ALREADY_PRACTICE_PROCESS: {
+        code: 'STUDENT_HAS_ALREADY_PRACTICE_PROCESS',
+        message: 'Student already has a practice process assigned.',
+        status: 400,
+    },
+}
+
+
 export const PRACTICE_PROCESS_DELIVERABLE_CONSTRAINTS = {
     GRADE: {
         MIN: 0,
