e7fb023ab852d03f4ae003145f0ae4188862d80a
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-13 22:37:55 -0500

    feat(practice-process): register controllers, mongoose schemas and dependencies

--- src/practice-process/practice-process.module.ts
+++ src/practice-process/practice-process.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { PracticeProcessService } from './services/practice-process.service';
+import { PracticeProcessController } from './controllers/practice-process.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PracticeProcess, PracticeProcessSchema } from './schemas/practice-process.schema';
+import { PracticeProcessDeliverable, PracticeProcessDeliverableSchema } from './schemas/practice-process-deliverable.schema';
+import { PracticeDefinitionModule } from 'practice-definition/practice-definition.module';
+import { ProfessorModule } from 'professor/professor.module';
+import { StudentModule } from 'student/student.module';
+import { CompanyModule } from 'company/company.module';
+import { PracticeProcessMapper } from './mappers/practice-process.mapper';
+import { AuthModule } from '@auth/auth.module';
 
 @Module({
-  providers: [PracticeProcessService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: PracticeProcess.name,
+        schema: PracticeProcessSchema,
+      },
+      {
+        name: PracticeProcessDeliverable.name,
+        schema: PracticeProcessDeliverableSchema,
+      }
+    ]),
+    PracticeDefinitionModule,
+    ProfessorModule,
+    StudentModule,
+    CompanyModule,
+    AuthModule
+  ],
+  providers: [PracticeProcessService, PracticeProcessMapper],
+  controllers: [PracticeProcessController]
 })
-export class PracticeProcessModule {}
+export class PracticeProcessModule { }
