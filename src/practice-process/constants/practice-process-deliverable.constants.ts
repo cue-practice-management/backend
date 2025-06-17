21897e438484d8eef935e89be371aa58b26bb7ac
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-16 21:31:10 -0500

    feat(practice-process): add exception codes and messages for deliverable status and not found

--- src/practice-process/constants/practice-process-deliverable.constants.ts
+++ src/practice-process/constants/practice-process-deliverable.constants.ts
@@ -1,3 +1,18 @@
+import { HttpStatus } from "@nestjs/common"
+
+export const PRACTICE_PROCESS_DELIVERABLE_EXCEPTIONS = {
+    NOT_FOUND: {
+        code: 'PRACTICE_PROCESS_DELIVERABLE_NOT_FOUND',
+        message: 'Practice process deliverable not found.',
+        status: HttpStatus.NOT_FOUND,
+    },
+    INVALID_STATUS_TO_SUBMIT: {
+        code: 'PRACTICE_PROCESS_DELIVERABLE_INVALID_STATUS_TO_SUBMIT',
+        message: 'Deliverable cannot be submitted in its current status.',
+        status: HttpStatus.BAD_REQUEST,
+    }
+}
+
 export const PRACTICE_PROCESS_DELIVERABLE_CONSTRAINTS = {
     GRADE: {
         MIN: 0,
