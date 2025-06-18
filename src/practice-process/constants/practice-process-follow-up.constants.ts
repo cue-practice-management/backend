a0a15d75f60af4d60f91f1369a2e800a3d42336b
Juan Gerardo <jmendez1025@cue.edu.co>
2025-06-18 11:53:28 -0500

    feat(practice-process-follow-up): schedule, cancel, complete follow ups

--- src/practice-process/constants/practice-process-follow-up.constants.ts
+++ src/practice-process/constants/practice-process-follow-up.constants.ts
@@ -16,5 +16,20 @@ export const PRACTICE_PROCESS_FOLLOW_UP_EXCEPTIONS = {
         code: 'PRACTICE_PROCESS_FOLLOW_UP_PROFESSOR_HAS_ALREADY_FOLLOW_UP_ON_DATE',
         message: 'Professor has already scheduled a follow-up on this date.',
         status: HttpStatus.BAD_REQUEST,
-    }
+    },
+    INVALID_STATUS_TO_CANCEL: {
+        code: 'PRACTICE_PROCESS_FOLLOW_UP_INVALID_STATUS_TO_CANCEL',
+        message: 'Follow-up cannot be canceled in current status.',
+        status: HttpStatus.BAD_REQUEST,
+    },
+    INVALID_STATUS_TO_COMPLETE: {
+        code: 'PRACTICE_PROCESS_FOLLOW_UP_INVALID_STATUS_TO_COMPLETE',
+        message: 'Follow-up cannot be completed in current status.',
+        status: HttpStatus.BAD_REQUEST,
+    },
+    INVALID_DATE_TO_COMPLETE: {
+        code: 'PRACTICE_PROCESS_FOLLOW_UP_INVALID_DATE_TO_COMPLETE',
+        message: 'Follow-up cannot be completed on this date, since it is in the past.',
+        status: HttpStatus.BAD_REQUEST,
+    },
 }
--- src/practice-process/services/practice-process-follow-up.service.ts
+++ src/practice-process/services/practice-process-follow-up.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel, Types } from 'mongoose';
+import { CancelPracticeProcessFollowUpRequestDto } from 'practice-process/dtos/cancel-practice-process-follo-up-request.dto';
+import { CompletePracticeProcessFollowUpRequestDto } from 'practice-process/dtos/complete-practice-process-follo-up-request.dto';
 import { PracticeProcessFollowUpResponseDto } from 'practice-process/dtos/practice-process-follow-up-response.dto';
 import { ProfessorHasAlreadyFollowUpOnDateException } from 'practice-process/dtos/professor-has-already-follow-up-on-date.exception';
 import { SchedulePracticeProcessFollowUpRequestDto } from 'practice-process/dtos/schedule-practice-process-follow-up-request.dto';
 import { FollowUpStatus } from 'practice-process/enums/practice-process-follow-up.enum';
 import { PracticeProcessStatus } from 'practice-process/enums/practice-process.enums';
+import { PracticeProcessFollowUpInvalidDateToCompleteException } from 'practice-process/exceptions/practice-process-follow-up-invalid-date-to-complete.exception';
+import { PracticeProcessFollowUpInvalidStatusToCancelException } from 'practice-process/exceptions/practice-process-follow-up-invalid-status-to-cancel.exception';
+import { PracticeProcessFollowUpInvalidStatusToCompleteException } from 'practice-process/exceptions/practice-process-follow-up-invalid-status-to-complete.exception';
 import { PracticeProcessFollowUpInvalidStatusToScheduleException } from 'practice-process/exceptions/practice-process-follow-up-invalid-status-to-schedule.exception';
 import { PracticeProcessFollowUpNotAuthorizedForScheduleException } from 'practice-process/exceptions/practice-process-follow-up-not-authorized-for-schedule.exception';
 import { PracticeProcessNotFoundException } from 'practice-process/exceptions/practice-process-not-found.exception';
 import { PracticeProcessFollowUpMapper } from 'practice-process/mappers/practice-process-follow-up.mapper';
-import { PracticeProcessFollowUp } from 'practice-process/schemas/practice-process-follow-up.schema';
-import { PracticeProcess } from 'practice-process/schemas/practice-process.schema';
+import { PracticeProcessFollowUp, PracticeProcessFollowUpDocument } from 'practice-process/schemas/practice-process-follow-up.schema';
+import { PracticeProcess, PracticeProcessDocument } from 'practice-process/schemas/practice-process.schema';
 
 @Injectable()
 export class PracticeProcessFollowUpService {
 
     constructor(
         @InjectModel(PracticeProcessFollowUp.name)
-        private readonly practiceProcessFollowUpModel: PaginateModel<PracticeProcessFollowUp>,
+        private readonly practiceProcessFollowUpModel: PaginateModel<PracticeProcessFollowUpDocument>,
         @InjectModel(PracticeProcess.name)
-        private readonly practiceProcessModel: PaginateModel<PracticeProcess>,
+        private readonly practiceProcessModel: PaginateModel<PracticeProcessDocument>,
         private readonly practiceProcessFollowUpMapper: PracticeProcessFollowUpMapper
     ) { }
 
@@ -30,10 +35,12 @@ export class PracticeProcessFollowUpService {
         userId: string,
         scheduleFollowUpRequestDto: SchedulePracticeProcessFollowUpRequestDto
     ): Promise<PracticeProcessFollowUpResponseDto> {
-
         const practiceProcess = await this.validatePracticeProcessExists(practiceProcessId);
+
         await this.validateUserIsProfessorOfPracticeProcess(practiceProcess, userId);
-        await this.validatePracticeProcessStatus(practiceProcess);
+
+        this.validatePracticeProcessStatusToSchedule(practiceProcess);
+
         await this.validateProfessorHasNotFollowUpOnDate(scheduleFollowUpRequestDto.date, userId);
 
         const practiceProcessFollowUp = new this.practiceProcessFollowUpModel({
@@ -47,6 +54,50 @@ export class PracticeProcessFollowUpService {
         return this.practiceProcessFollowUpMapper.toResponseDto(practiceProcessFollowUp);
     }
 
+    async cancelFollowUp(
+        practiceProcessId: string,
+        followUpId: string,
+        userId: string,
+        cancelPracticeProcessFollowUpRequestDto: CancelPracticeProcessFollowUpRequestDto
+    ): Promise<PracticeProcessFollowUpResponseDto> {
+        const practiceProcess = await this.validatePracticeProcessExists(practiceProcessId);
+        await this.validateUserIsProfessorOfPracticeProcess(practiceProcess, userId);
+
+        const followUp = await this.validatePracticeProcessFollowUpExists(practiceProcessId, followUpId);
+        this.validatePracticeProcessFollowUpStatusToCancel(followUp);
+
+        followUp.status = FollowUpStatus.CANCELLED;
+        followUp.cancellationReason = cancelPracticeProcessFollowUpRequestDto.cancellationReason;
+        followUp.cancelledAt = new Date();
+
+        await followUp.save();
+
+        return this.practiceProcessFollowUpMapper.toResponseDto(followUp);
+    }
+
+    async completeFollowUp(
+        practiceProcessId: string,
+        followUpId: string,
+        userId: string,
+        completeFollowUpRequestDto: CompletePracticeProcessFollowUpRequestDto
+    ): Promise<PracticeProcessFollowUpResponseDto> {
+        const practiceProcess = await this.validatePracticeProcessExists(practiceProcessId);
+        await this.validateUserIsProfessorOfPracticeProcess(practiceProcess, userId);
+
+        const followUp = await this.validatePracticeProcessFollowUpExists(practiceProcessId, followUpId);
+
+        this.validatePracticeProcessFollowUpStatusToComplete(followUp);
+        this.validatePracticeProcessFollowUpDateToComplete(followUp);
+        
+        followUp.status = FollowUpStatus.COMPLETED;
+        followUp.outcomeNotes = completeFollowUpRequestDto.outcomeNotes;
+        followUp.completedAt = new Date();
+
+        await followUp.save();
+
+        return this.practiceProcessFollowUpMapper.toResponseDto(followUp);
+    }
+
     private async validatePracticeProcessExists(practiceProcessId: string): Promise<PracticeProcess> {
         const practiceProcess = await this.practiceProcessModel.findById(practiceProcessId);
 
@@ -57,10 +108,39 @@ export class PracticeProcessFollowUpService {
         return practiceProcess;
     }
 
-    private async validatePracticeProcessStatus(practiceProcess: PracticeProcess): Promise<void> {
+    private validatePracticeProcessStatusToSchedule(practiceProcess: PracticeProcess): void {
         if (practiceProcess.status !== PracticeProcessStatus.IN_PROGRESS) {
             throw new PracticeProcessFollowUpInvalidStatusToScheduleException();
         }
+    }
+
+    private validatePracticeProcessFollowUpDateToComplete(followUp: PracticeProcessFollowUp): void {
+        if (new Date() < new Date(followUp.date)) {
+            throw new PracticeProcessFollowUpInvalidDateToCompleteException();
+        }
+    }
+
+    private validatePracticeProcessFollowUpStatusToComplete(followUp: PracticeProcessFollowUp): void {
+        if (followUp.status !== FollowUpStatus.SCHEDULED) {
+            throw new PracticeProcessFollowUpInvalidStatusToCompleteException();
+        }
+    }
+
+    private validatePracticeProcessFollowUpStatusToCancel(followUp: PracticeProcessFollowUp): void {
+        if (followUp.status !== FollowUpStatus.SCHEDULED) {
+            throw new PracticeProcessFollowUpInvalidStatusToCancelException();
+        }
+    }
+
+    private async validatePracticeProcessFollowUpExists(practiceProcessId: string, followUpId: string): Promise<PracticeProcessFollowUpDocument> {
+        const followUp = await this.practiceProcessFollowUpModel.findOne({
+            _id: followUpId,
+            process: practiceProcessId,
+        });
+
+        if (!followUp) throw new PracticeProcessNotFoundException();
+
+        return followUp;
     }
 
     private async validateUserIsProfessorOfPracticeProcess(
